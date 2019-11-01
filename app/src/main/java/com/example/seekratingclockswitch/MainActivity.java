d184f3a58f0df01b7294245698080f87ebbfa8d3
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-01 16:48:25 +0630

    creating activity files and inserting associated intent to go to associated screen

--- app/src/main/java/com/example/seekratingclockswitch/MainActivity.java
+++ app/src/main/java/com/example/seekratingclockswitch/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.seekratingclockswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSeekBarActivity(View view){
+        Intent seekbarIntent = new Intent(MainActivity.this,SeekBarActivity.class);
+        startActivity(seekbarIntent);
+    }
+
+    public void goToRatingBarActivity(View view){
+        Intent ratingBarIntent = new Intent(MainActivity.this,RatingBarActivity.class);
+        startActivity(ratingBarIntent);
+    }
+
+    public void goToSwitchActivity(View view){
+        Intent switchIntent = new Intent(MainActivity.this,SwitchActivity.class);
+        startActivity(switchIntent);
+    }
+
+    public void goToTextClockActivity(View view){
+        Intent textClockIntent = new Intent(MainActivity.this,TextClockActivity.class);
+        startActivity(textClockIntent);
     }
 }
