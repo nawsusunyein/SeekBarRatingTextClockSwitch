002f589fa0febed3bbd349c6e6cfff47d82a5db4
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-01 17:30:24 +0630

    insert textclock activity and xml file

--- app/src/main/java/com/example/seekratingclockswitch/TextClockActivity.java
+++ app/src/main/java/com/example/seekratingclockswitch/TextClockActivity.java
@@ -3,6 +3,10 @@ package com.example.seekratingclockswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class TextClockActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class TextClockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_clock);
+        final TextClock textClock = (TextClock) findViewById(R.id.textClock1);
+        Button btnGetTime = (Button) findViewById(R.id.btnGet);
+        final TextView txtTime = (TextView) findViewById(R.id.txtTime);
+
+        btnGetTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTime.setText(textClock.getText());
+            }
+        });
     }
 }
