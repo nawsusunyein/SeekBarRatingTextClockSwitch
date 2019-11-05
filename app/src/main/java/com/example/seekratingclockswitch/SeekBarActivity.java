8efe7ed9a5180205f9b6c8830b2503dd69a6ce9e
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-05 09:36:05 +0630

    adding progress bar xml and activity

--- app/src/main/java/com/example/seekratingclockswitch/SeekBarActivity.java
+++ app/src/main/java/com/example/seekratingclockswitch/SeekBarActivity.java
@@ -3,6 +3,9 @@ package com.example.seekratingclockswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SeekBarActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class SeekBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek_bar);
+
+        SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
+        final TextView txtSeekView = (TextView) findViewById(R.id.seekbarTextview);
+        final int progress = seekbar.getProgress();
+       // txtSeekView.setText(progress);
+        //txtSeekView.setText(seekbar.getProgress());
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+                txtSeekView.setText(pval + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(SeekBarActivity.this,"Progress is starting...",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                    txtSeekView.setText(pval + "/" + seekBar.getMax());
+            }
+        });
     }
 }
